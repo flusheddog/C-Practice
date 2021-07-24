87c55f6528ceab4b2f05aa64a33ee2196606f684
flusheddog <lukegtseng@gmail.com>
2021-07-24 00:44:57 -0700

    Update Squeeze.c

--- Squeeze.c
+++ Squeeze.c
@@ -1,26 +1,23 @@
-  
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void squeeze (char a[], char b[]) {
-  int i,j,k;
-  for(k = 0; b[k] != '\0'; k++) {
+void squeeze (char a[], char b) {
+  int i,j;
     for(i = j = 0; a[i]!= '\0'; i++) {
-      if(a[i] != b[k]) {
+      if(a[i] != b) {
 	    a[j++] = a[i];
       }
     }
     a[j] ='\0';
-  }
 }
 
-int main() { 
-
+int main() {
   char str1[10000];
   char str2[10000];
   scanf("%s %s", str1, str2);
-  squeeze(str1, str2);
+  for(int i = 0; str2[i] != '\0'; i++) {
+    squeeze(str1, str2[i]);
+  }
   printf("%s\n", str1);
     return 0;
 }
