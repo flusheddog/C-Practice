caa358ecb15c82e6eda6b85f7d119f261a3f4c65
Luke Tseng <lukegtseng@gmail.com>
2021-07-23 23:27:24 -0700

    Create Squeeze.c

---
+++ Squeeze.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void squeeze (char a[], char b[]) {
+  int i,j,k;
+  for(k = 0; b[k] != '\0'; k++) {
+    for(i = j = 0; a[i]!= '\0'; i++){
+      if(a[i] != b[k]) {
+	    a[j++] = a[i];
+      }
+    }
+  }
+  a[j] ='\0';
+
+  
+}
+
+int main() { 
+
+  char str1[] = "hello";
+  char str2[] = "l";
+  squeeze(str1, str2);
+  printf("%s\n", str1);
+    return 0;
+}
